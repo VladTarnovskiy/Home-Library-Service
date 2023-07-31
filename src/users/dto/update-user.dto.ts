083cef63eaf6b84bf52b620c2923e3e7be869349
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-07-31 03:03:14 +0300

    feat: add validation

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,6 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 
-export class UpdatePasswordDto {
+export class UpdateUserDto {
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UserStorage } from './storage/users.storage';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UserStorage],
 })
 export class UsersModule {}
