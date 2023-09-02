58488dff32cbdc865c01ac96615f623b99e2678b
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-09-02 22:18:04 +0300

    feat: add logger handlers

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,24 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const logger = app.get(LoggingService);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  app.useLogger(app.get(LoggingService));
+  app.useLogger(logger);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT || 4000);
+
+  process.on('unhandledRejection', (err) => {
+    logger.error(`Unhandled Rejection: ${JSON.stringify(err)}`);
+  });
+
+  process.on('uncaughtException', (err) => {
+    const message = err instanceof Error ? err.message : JSON.stringify(err);
+    logger.error(`Uncaught Exception: ${message}`);
+  });
 }
 bootstrap();
