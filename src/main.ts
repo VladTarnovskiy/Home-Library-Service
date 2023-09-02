f8bb273c000fe64909e822670362576eba82f9e3
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-09-02 22:29:20 +0300

    feat: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { LoggingService } from './logger/logger.service';
+import { HttpExceptionFilter } from './utils/exeptionFilter';
 dotenv.config();
 
 async function bootstrap() {
@@ -16,6 +17,8 @@ async function bootstrap() {
   );
   app.useLogger(logger);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT || 4000);
 
   process.on('unhandledRejection', (err) => {
